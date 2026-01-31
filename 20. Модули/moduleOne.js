406c7244f24050928825fec0fd0b0e3e026f867b
Sergey Rudenko <sergeyrudenkosr@gmail.com>
2026-01-31 15:01:32 +0200

    20.3 Несколько экспортов

--- 20. Модули/moduleOne.js
+++ 20. Модули/moduleOne.js
@@ -1,7 +1,14 @@
+/*
 const myName = () => {
   console.log("Sergey");
 };
+*/
 
 // Название переменных могут не совпадать
 // Экспорт по умолчанию
 export default myName;
+
+const one = 1;
+const two = "two";
+
+export { one, two };
--- 20. Модули/moduleTwo.js
+++ 20. Модули/moduleTwo.js
@@ -1,4 +1,11 @@
-// Название переменных могут не совпадать
+/*
+//Название переменных могут не совпадать
 import printMyName from "./moduleOne.js";
 
 printMyName();
+*/
+
+import { one as oneRenamed, two } from "./moduleOne.js";
+
+console.log(oneRenamed);
+console.log(two);
