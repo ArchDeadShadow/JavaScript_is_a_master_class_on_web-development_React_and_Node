982be5364c6ab7db950b78d0ca290778a2cd9f3f
Sergey Rudenko <sergeyrudenkosr@gmail.com>
2026-02-04 18:41:12 +0200

    UPD: Упростил вызов промиса в index.js файле

--- 22. Промисы/index.js
+++ 22. Промисы/index.js
@@ -1,3 +1,4 @@
+/*
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => {
     console.log(response);
@@ -5,3 +6,16 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
   })
   .then((json) => console.log(json))
   .catch((error) => console.error(error));
+*/
+
+const getData = (url) =>
+  new Promise((resolve, reject) =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => resolve(json))
+      .catch((error) => reject(error))
+  );
+
+getData("https://jsonplaceholder.typicode.com/todos")
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error.message));
